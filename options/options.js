ea2e4752dd8923bb0f2893a3ec79edb2a801777c
Crowdbotics <engineering@crowdbotics.com>
2023-03-11 01:36:07 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "broken-butterfly-de-68766",
+  url: "https://broken-butterfly-de-68766.botics.co",
+  api: "https://broken-butterfly-de-68766.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
